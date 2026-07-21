3ab06c544f919481ba4854ddd0a55edc7676ed32
Marvelous-dot <234076621@qq.com>
2026-07-21 12:05:24 +0800

    Upload: frontend/src/App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AppLayout from './components/AppLayout.vue'
+</script>
+
+<template>
+  <AppLayout>
+    <router-view />
+  </AppLayout>
+</template>
+
+<style scoped>
+.app-main {
+  max-width: 1400px;
+  margin: 0 auto;
+  padding: 0 24px;
+}
+</style>
